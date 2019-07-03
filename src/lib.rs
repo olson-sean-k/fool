ac1a42847c441b1737c0febfad18c2855b7a1a5c
Sean Olson <olson.sean.k@gmail.com>
2019-07-03 13:28:00 -0700

    Remove `and_if` for `Option` and rename `bool` conversions.

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,18 @@
 //!
 //! # Examples
 //!
-//! Using `and_if` to produce an `Option` if a predicate succeeds:
+//! Using `and_if` to produce an `Option` from a `Result` if a predicate
+//! succeeds:
 //!
 //! ```rust
 //! use fool::prelude::*;
 //!
-//! pub fn get<T>() -> Option<T> {
+//! pub fn try_get<T>() -> Result<T, ()> {
 //!     // ...
-//!     # None
+//!     # Err(())
 //! }
 //!
-//! if let Some(value) = get::<u32>().and_if(|value| *value > 10) {
+//! if let Some(value) = try_get::<u32>().and_if(|value| *value > 10) {
 //!     // ...
 //! }
 //! ```
@@ -38,46 +39,46 @@
 extern crate std;
 
 pub trait BoolExt: Sized {
-    fn option(self) -> Option<()>;
+    fn into_option(self) -> Option<()>;
 
-    fn result(self) -> Result<(), ()>;
+    fn into_result(self) -> Result<(), ()>;
 
     fn and<T>(self, option: Option<T>) -> Option<T> {
-        self.option().and(option)
+        self.into_option().and(option)
     }
 
     fn and_then<T, F>(self, f: F) -> Option<T>
     where
         F: Fn() -> Option<T>,
     {
-        self.option().and_then(move |_| f())
+        self.into_option().and_then(move |_| f())
     }
 
     fn some<T>(self, value: T) -> Option<T> {
-        self.option().map(move |_| value)
+        self.into_option().map(move |_| value)
     }
 
     fn some_with<T, F>(self, f: F) -> Option<T>
     where
         F: Fn() -> T,
     {
-        self.option().map(move |_| f())
+        self.into_option().map(move |_| f())
     }
 
     fn ok_or<E>(self, error: E) -> Result<(), E> {
-        self.result().map_err(move |_| error)
+        self.into_result().map_err(move |_| error)
     }
 
     fn ok_or_else<E, F>(self, f: F) -> Result<(), E>
     where
         F: Fn() -> E,
     {
-        self.result().map_err(move |_| f())
+        self.into_result().map_err(move |_| f())
     }
 }
 
 impl BoolExt for bool {
-    fn option(self) -> Option<()> {
+    fn into_option(self) -> Option<()> {
         if self {
             Some(())
         }
@@ -86,7 +87,7 @@ impl BoolExt for bool {
         }
     }
 
-    fn result(self) -> Result<(), ()> {
+    fn into_result(self) -> Result<(), ()> {
         if self {
             Ok(())
         }
@@ -96,19 +97,19 @@ impl BoolExt for bool {
     }
 }
 
-pub trait OptionExt<T> {
-    fn and_if<F>(self, f: F) -> Self
+pub trait ResultExt<T, E> {
+    fn and_if<F>(self, f: F) -> Option<T>
     where
         F: Fn(&T) -> bool;
 }
 
-impl<T> OptionExt<T> for Option<T> {
-    fn and_if<F>(mut self, f: F) -> Self
+impl<T, E> ResultExt<T, E> for Result<T, E> {
+    fn and_if<F>(self, f: F) -> Option<T>
     where
         F: Fn(&T) -> bool,
     {
-        match self.take() {
-            Some(value) => {
+        match self {
+            Ok(value) => {
                 if f(&value) {
                     Some(value)
                 }
