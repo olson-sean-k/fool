2543372f141eef929e1b4f126343933b5ce64c1e
Sean Olson <olson.sean.k@gmail.com>
2019-08-16 11:55:13 -0700

    Implement Boolean coercions and macros.

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,10 @@
 #[cfg(feature = "std")]
 extern crate std;
 
+pub trait IntoBool {
+    fn into_bool(self) -> bool;
+}
+
 pub trait BoolExt: Sized {
     fn into_option(self) -> Option<()>;
 
@@ -97,10 +101,52 @@ impl BoolExt for bool {
     }
 }
 
+impl IntoBool for bool {
+    fn into_bool(self) -> bool {
+        self
+    }
+}
+
+impl<T> IntoBool for Option<T> {
+    fn into_bool(self) -> bool {
+        self.is_some()
+    }
+}
+
+impl<'a, T> IntoBool for &'a Option<T> {
+    fn into_bool(self) -> bool {
+        self.is_some()
+    }
+}
+
+impl<'a, T> IntoBool for &'a mut Option<T> {
+    fn into_bool(self) -> bool {
+        self.is_some()
+    }
+}
+
 pub trait ResultExt<T, E> {
     fn and_if<F>(self, f: F) -> Option<T>
     where
         F: Fn(&T) -> bool;
+}
+
+impl<T, E> IntoBool for Result<T, E> {
+    fn into_bool(self) -> bool {
+        self.is_ok()
+    }
+}
+
+impl<'a, T, E> IntoBool for &'a Result<T, E> {
+    fn into_bool(self) -> bool {
+        self.is_ok()
+    }
+}
+
+impl<'a, T, E> IntoBool for &'a mut Result<T, E> {
+    fn into_bool(self) -> bool {
+        self.is_ok()
+    }
 }
 
 impl<T, E> ResultExt<T, E> for Result<T, E> {
@@ -122,6 +168,139 @@ impl<T, E> ResultExt<T, E> for Result<T, E> {
     }
 }
 
+#[macro_export]
+macro_rules! and {
+    ($head:expr, $($tail:expr),*$(,)?) => ({
+        use $crate::IntoBool;
+
+        $head.into_bool() $(&& ($tail.into_bool()))*
+    });
+    (option => $head:expr, $($tail:expr),*$(,)?) => (
+        $head$(.and($tail))*
+    );
+    (result => $head:expr, $($tail:expr),*$(,)?) => (
+        $head$(.and($tail))*
+    );
+}
+
+#[macro_export]
+macro_rules! or {
+    ($head:expr, $($tail:expr),*$(,)?) => ({
+        use $crate::IntoBool;
+
+        $head.into_bool() $(|| ($tail.into_bool()))*
+    });
+    (option => $head:expr, $($tail:expr),*$(,)?) => (
+        $head$(.or($tail))*
+    );
+    (result => $head:expr, $($tail:expr),*$(,)?) => (
+        $head$(.or($tail))*
+    );
+}
+
+#[macro_export]
+macro_rules! xor {
+    ($head:expr, $($tail:expr),*$(,)?) => ({
+        use $crate::IntoBool;
+
+        $head.into_bool() $(^ ($tail.into_bool()))*
+    });
+    (option => $head:expr, $($tail:expr),*$(,)?) => (
+        $head$(.xor($tail))*
+    );
+}
+
 pub mod prelude {
-    pub use super::*;
+    pub use crate::{BoolExt, ResultExt};
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{and, or, xor};
+
+    fn some() -> Option<u32> {
+        Some(0)
+    }
+
+    fn none() -> Option<u32> {
+        None
+    }
+
+    fn ok() -> Result<u32, ()> {
+        Ok(0)
+    }
+
+    fn err() -> Result<u32, ()> {
+        Err(())
+    }
+
+    #[test]
+    fn and() {
+        assert!(and!(true, true, true));
+        assert!(!and!(true, true, false));
+
+        assert!(and!(some(), some(), some()));
+        assert!(!and!(some(), some(), none()));
+
+        assert!(and!(ok(), ok(), ok()));
+        assert!(!and!(ok(), ok(), err()));
+    }
+
+    #[test]
+    fn and_option() {
+        assert!(and!(option => some(), some(), some()).is_some());
+        assert!(and!(option => some(), some(), none()).is_none());
+    }
+
+    #[test]
+    fn and_result() {
+        assert!(and!(result => ok(), ok(), ok()).is_ok());
+        assert!(and!(result => ok(), ok(), err()).is_err());
+    }
+
+    #[test]
+    fn or() {
+        assert!(or!(true, false, false));
+        assert!(!or!(false, false, false));
+
+        assert!(or!(some(), none(), none()));
+        assert!(!or!(none(), none(), none()));
+
+        assert!(or!(ok(), err(), err()));
+        assert!(!or!(err(), err(), err()));
+    }
+
+    #[test]
+    fn or_option() {
+        assert!(or!(option => some(), some(), none()).is_some());
+        assert!(or!(option => none(), none(), none()).is_none());
+    }
+
+    #[test]
+    fn or_result() {
+        assert!(or!(result => ok(), ok(), err()).is_ok());
+        assert!(or!(result => err(), err(), err()).is_err());
+    }
+
+    #[test]
+    fn xor() {
+        assert!(xor!(true, false, false));
+        assert!(!xor!(true, true, false));
+        assert!(!xor!(false, false, false));
+
+        assert!(xor!(some(), none(), none()));
+        assert!(!xor!(some(), some(), none()));
+        assert!(!xor!(none(), none(), none()));
+
+        assert!(xor!(ok(), err(), err()));
+        assert!(!xor!(ok(), ok(), err()));
+        assert!(!xor!(err(), err(), err()));
+    }
+
+    #[test]
+    fn xor_option() {
+        assert!(xor!(option => some(), none(), none()).is_some());
+        assert!(xor!(option => some(), some(), none()).is_none());
+        assert!(xor!(option => none(), none(), none()).is_none());
+    }
 }
